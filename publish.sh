9a4cf12c3c0d6a6f87f2136378ed698ab6bd269f
AllenDK <allendk@foxmail.com>
2020-11-05 17:09:32 +0800

    fix url issue

--- publish.sh
+++ publish.sh
@@ -1,3 +1,8 @@
 flutter build web --release
-rm -rf excel_parser
-cp -r build/web/ excel_parser/
+cd build/web
+git init
+git remote add origin git@github.com:GameHolics/excel_parser.git
+git checkout -b gh-pages
+git add --all
+git commit -m "update"
+git push origin gh-pages -f
