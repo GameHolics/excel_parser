46efdadaca3ba9a1754971f98f46f3607d9395b7
AllenDK <allendk@foxmail.com>
2020-11-06 09:08:45 +0800

    update

--- publish.sh
+++
@@ -1,8 +0,0 @@
-flutter build web --release
-cd build/web
-git init
-git remote add origin git@github.com:GameHolics/excel_parser.git
-git checkout -b gh-pages
-git add --all
-git commit -m "update"
-git push origin gh-pages -f
